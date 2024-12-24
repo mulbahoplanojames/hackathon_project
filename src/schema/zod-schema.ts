3fdb92bd342c93ddb864a1b80037ecd3cc4bde2c
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-12-25 01:04:10 +0200

    Added zod validation schema for the form

---
+++ src/schema/zod-schema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+export const signupSchema = z.object({
+  firstName: z
+    .string()
+    .min(3, { message: "First Name must be at least 2 characters long" }),
+  lastName: z
+    .string()
+    .min(3, { message: "Last Name must be at least 2 characters long" }),
+  email: z.string().email(),
+  rollNumber: z
+    .string()
+    .min(8, { message: "Roll Number must be at least 8 characters long" }),
+  phoneNumber: z
+    .string()
+    .min(10, { message: "Phone Number must be at least 10 characters long" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
