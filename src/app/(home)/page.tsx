c95c51443d05b08c0b030402756b81c23ae25b50
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-12-25 16:56:50 +0200

    updated the repo, 95% done with the ourVision component📂

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import HomeHero from "@/components/Home/HomeHero";
+import OurVision from "@/components/Home/OurVision";
 
 const Home = () => {
   return (
     <>
       <section className="w-full">
         <HomeHero />
+        <OurVision />
       </section>
     </>
   );
