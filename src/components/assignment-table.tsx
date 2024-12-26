12fd34624c70ab7809d2ef8e873deca8329b8bbd
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-12-26 16:16:28 +0200

    updated the repo 📂, completed all of the components with in the  assignments route

--- src/components/assignment-table.tsx
+++ src/components/assignment-table.tsx
@@ -10,7 +10,6 @@ import {
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Badge } from "@/components/ui/badge";
 import { assignmentData } from "@/constant/assignmentData";
 import { FolderUp, Printer } from "lucide-react";
 
