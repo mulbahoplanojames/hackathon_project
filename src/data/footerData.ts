7b839b559149ff63d59bc4592acd52858da40cfa
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2025-01-01 20:56:37 +0200

    updated the repo 📂, added the footer

---
+++ src/data/footerData.ts
@@ -0,0 +1,30 @@
+export const footerData = [
+  {
+    title: "Resourses",
+    links: [
+      { name: "Academic Support", href: "#" },
+      { name: "Health Resources", href: "#" },
+      { name: "E-Commerce", href: "#" },
+      { name: "Community Engagement", href: "#" },
+      { name: "Partners", href: "#" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { name: "Home", href: "#" },
+      { name: "About", href: "#" },
+      { name: "Team", href: "#" },
+      { name: "Signup", href: "#" },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      { name: "Help Center", href: "#" },
+      { name: "Safety Center", href: "#" },
+      { name: "Community Guidelines", href: "#" },
+      { name: "Feedback", href: "#" },
+    ],
+  },
+];
