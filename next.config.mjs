b2b702348ec1edd6e039f60ce11c37e4ce53e80f
mubarak <luismubarak@gmail.com>
2025-01-17 18:02:20 +0200

    make image url

--- next.config.mjs
+++ next.config.mjs
@@ -11,11 +11,25 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
+        port: "8000",
+        pathname: '/storage/**',
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+        port: "",
+        pathname: "/**",
+      },
+
+      {
+        protocol: "https",
+        hostname: "images.app.goo.gl",
         port: "",
         pathname: "/**",
       },
     ],
   },
 };
+
 
 export default nextConfig;
